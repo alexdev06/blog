87cd8ac8fe53f3f78f32707320e0b4b45181a5dc
alexdev06 <alexandre.manteaux@gmail.com>
2019-11-17 14:59:19 +0100

    First iteration of the Inscription Frontend module. The form works. Appearance is minimal, just for testing.

---
+++ src/App/Frontend/Modules/Inscription/Views/inscRequest.php
@@ -0,0 +1,27 @@
+<section class="content-page" style="margin-top: 150px">
+    <h2 class="text-center"><?= $title ?></h2>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="name">Prénom :</label><br />
+            <input type="text" id="name" name="name" class="form-control" /><br />
+        </div>
+        <div class="form-group">
+            <label for="last_name">Nom :</label><br />
+            <input type="text" id="last_name" name="last_name" class="form-control" /><br />
+        </div>
+        <div class="form-group">
+            <label for="username">Pseudo :</label><br />
+            <input type="text" id="username" name="username" class="form-control" /><br />
+        </div>
+        <div class="form-group">
+            <label for="email">Email :</label><br />
+            <input type="email" id="email" name="email"  class="form-control"/><br />
+        </div>
+        <div class="form-group">
+            <label for="password">Mot de passe :</label><br />
+            <input type="password" id="password" name="password" class="form-control" /><br />
+        </div>
+        <input class="btn btn-success btn-lg" type="submit" value="Envoyer" />
+        <input class="btn btn-success btn-lg" type="reset" value="Réinitialiser">
+    </form> 
+</section>
