4789940472cc514173cbe7c89827daf771fd0f4a
alexdev06 <alexandre.manteaux@gmail.com>
2019-11-18 21:57:48 +0100

    Added link to the news

--- src/App/Backend/Modules/Comments/Views/index.php
+++ src/App/Backend/Modules/Comments/Views/index.php
@@ -5,17 +5,17 @@
             <tr><th>Auteur</th><th>News</th><th>Contenu</th><th>Date d'ajout</th><th>Status</th><th>Action</th></tr>
         </thead>
         <tbody>
-<?php
-foreach ($comments as $comment)
-{
-    if ($comment['published'] == 1) {
-        $comment['published'] = 'publié';
-    } else {
-        $comment['published'] = 'masqué';
-    }
-    echo '<tr><td>', $comment['author'], '</td><td>', $comment['news_id'], '</td><td>', nl2br(htmlspecialchars($comment['content'])), '</td><td>le ', $comment['date_create']->format('d/m/Y à H\hi'), '</td><td>' . $comment['published'] . '</td><td><a href="admin-comments-update-', $comment['id'], '">Modifier</a><br /> <a onclick="return confirm(\'Valider votre choix?\');" href="admin-comments-delete-', $comment['id'], '">Supprimer</a></td></tr>', "\n";
-}
-?>
-  </tbody>
+            <?php
+            foreach ($comments as $comment)
+            {
+                if ($comment['published'] == 1) {
+                    $comment['published'] = 'publié';
+                } else {
+                    $comment['published'] = 'masqué';
+                }
+                echo '<tr><td>', $comment['author'], '</td><td><a href="/admin-news-', $comment['news_id'], '">', $comment['news_id'],'</a></td><td>', nl2br(htmlspecialchars($comment['content'])), '</td><td>le ', $comment['date_create']->format('d/m/Y à H\hi'), '</td><td>' . $comment['published'] . '</td><td><a href="admin-comments-update-', $comment['id'], '">Modifier</a><br /> <a onclick="return confirm(\'Valider votre choix?\');" href="admin-comments-delete-', $comment['id'], '">Supprimer</a></td></tr>', "\n";
+            }
+            ?>
+         </tbody>
     </table>
 </section>
