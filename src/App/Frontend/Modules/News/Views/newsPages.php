aac3b0bba69f95621a641ffc7bb6b9188f65c9e4
alexdev06 <alexandre.manteaux@gmail.com>
2019-11-25 11:40:19 +0100

    Integrated lead in the Frontend App.

--- src/App/Frontend/Modules/News/Views/newsPages.php
+++ src/App/Frontend/Modules/News/Views/newsPages.php
@@ -29,6 +29,8 @@
 
                 <h3><a href="news-<?= $news['id'] ?>"><?= htmlspecialchars($news['title']) ?></a></h3>
                 <p style="text-align: left;"><small><em>Modifiée le <?= $news['date_update']->format('d/m/Y à H\hi') ?></em></small></p>
+
+                <p class="lead"><?= $news['lead'] ?></p>
                 
                 <p><?= nl2br(htmlspecialchars($news['content'])) ?> <a href="news-<?= $news['id'] ?>">lire la suite</a></p>
                 <hr />
