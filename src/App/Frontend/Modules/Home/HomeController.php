f87101d37da2c4378f03fac97798aa4ae5da52cb
alexdev06 <alexandre.manteaux@gmail.com>
2019-12-18 13:43:59 +0100

    Added empty input checks

--- src/App/Frontend/Modules/Home/HomeController.php
+++ src/App/Frontend/Modules/Home/HomeController.php
@@ -30,39 +30,43 @@ class HomeController extends BackController
             $decode = json_decode(file_get_contents($api_url), true);
         
             if ($decode['success'] == true) {
-                $name = strip_tags(htmlspecialchars($request->postData('name')));
-                $lastName = strip_tags(htmlspecialchars($request->postData('lastName')));
-                $email_address = strip_tags(htmlspecialchars($request->postData('email')));
-                $message = strip_tags(htmlspecialchars($request->postData('message')));
+                if (empty($request->postData('name')) || empty($request->postData('lastName')) || empty($request->postData('email')) || empty($request->postData('message')) ) {
+                    $this->app->visitor()->setFlash('Tous les champs ne sont pas remplis!');
+                } else {
+                    $name = strip_tags(htmlspecialchars($request->postData('name')));
+                    $lastName = strip_tags(htmlspecialchars($request->postData('lastName')));
+                    $email_address = strip_tags(htmlspecialchars($request->postData('email')));
+                    $message = strip_tags(htmlspecialchars($request->postData('message')));
+                        
+                    // Create the email and send the message
                     
-                // Create the email and send the message
-                
-                $email_subject = "Demande de contact sur le blog par :  $name $lastName";
-                $email_body = "Vous avez reçu un nouveau message de contact en provenance de votre site.\n\n"."Les détails:\n\nPrénom: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+                    $email_subject = "Demande de contact sur le blog par :  $name $lastName";
+                    $email_body = "Vous avez reçu un nouveau message de contact en provenance de votre site.\n\n"."Les détails:\n\nPrénom: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 
-                $mail = new PHPMailer(true);
+                    $mail = new PHPMailer(true);
 
-                $mail->isSMTP();                                            // Send using SMTP
-                $mail->Host       = 'smtp.ionos.fr';                        // Set the SMTP server to send through
-                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'contact@alexandremanteaux.fr';         // SMTP username
-                $mail->Password   = '';                        // SMTP password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-                $mail->Port       = 587;                                    // TCP port to connect to
-                $mail->setFrom($email_address);
-                $mail->addAddress('contact@alexandremanteaux.fr');
-                $mail->isHTML(true);                                  // Set email format to HTML
-                $mail->Subject = $email_subject;
-                $mail->Body    = $email_body;
-                $mail->send();
-                $mail->ClearAddresses();
+                    $mail->isSMTP();                                            // Send using SMTP
+                    $mail->Host       = 'smtp.ionos.fr';                        // Set the SMTP server to send through
+                    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                    $mail->Username   = 'contact@alexandremanteaux.fr';         // SMTP username
+                    $mail->Password   = '';                        // SMTP password
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+                    $mail->Port       = 587;                                    // TCP port to connect to
+                    $mail->setFrom($email_address);
+                    $mail->addAddress('contact@alexandremanteaux.fr');
+                    $mail->isHTML(true);                                  // Set email format to HTML
+                    $mail->Subject = $email_subject;
+                    $mail->Body    = $email_body;
+                    $mail->send();
+                    $mail->ClearAddresses();
 
-                $mail->addAddress($email_address);
-                $mail->isHTML(true);
-                $mail->setFrom('noreply@alexandremanteaux.fr');
-                $mail->Subject = 'Demande de contact';
-                $mail->Body    = 'Votre demande de contact a bien été prise en compte. Vous serez contacté dans les plus brefs délais';
-                $mail->send();
+                    $mail->addAddress($email_address);
+                    $mail->isHTML(true);
+                    $mail->setFrom('noreply@alexandremanteaux.fr');
+                    $mail->Subject = 'Demande de contact';
+                    $mail->Body    = 'Votre demande de contact a bien été prise en compte. Vous serez contacté dans les plus brefs délais';
+                    $mail->send();
+                }
             }
         }
     }
--- src/App/Frontend/Modules/Registration/RegistrationController.php
+++ src/App/Frontend/Modules/Registration/RegistrationController.php
@@ -15,7 +15,7 @@ class RegistrationController extends BackController
         if ($request->postExists('login')) {
 
             // reCAPTCHA
-            $secret = "6LehGMAUAAAAAGT7FXQAvNN5APjP9d6mh7Qlp_rM";
+            /*$secret = "6LehGMAUAAAAAGT7FXQAvNN5APjP9d6mh7Qlp_rM";
             $response = $_POST['g-recaptcha-response'];
             $remoteip = $_SERVER['REMOTE_ADDR'];
             
@@ -26,33 +26,38 @@ class RegistrationController extends BackController
             
             $decode = json_decode(file_get_contents($api_url), true);
         
-            if ($decode['success'] == true) {
-                $pass = $request->postData('password');
-                $passCheck = $request->postData('passCheck');
-                if ($pass !== $passCheck) {
-                    $this->app->visitor()->setFlash('Les mots de passe sont différents!');
+            if ($decode['success'] == true) {}*/
+
+                if (empty($request->postData('name')) || empty($request->postData('lastName')) || empty($request->postData('email')) || empty($request->postData('login')) || empty($request->postData('password')) || empty($request->postData('passCheck')) ) {
+                    $this->app->visitor()->setFlash('Tous les champs ne sont pas remplis!');
                 } else {
-                    $pass = password_hash($pass, PASSWORD_DEFAULT);
-                    
-                    $user = new User([
-                        'name' => $request->postData('name'),
-                        'lastName' => $request->postData('lastName'),
-                        'username' => $request->postData('login'),
-                        'email' => $request->postData('email'),
-                        'password' => $pass,
-                    ]);
-                    $pseudo = $this->managers->getManagerOf('Users')->get($user->username());
-                    $email = $this->managers->getManagerOf('Users')->getEmail($user->email());
-                    if (isset($pseudo) && $pseudo != false) {
-                    $this->app->visitor()->setFlash('Le login n\'est plus disponible');
-                    } elseif (isset($email) && $email != false) {
-                        $this->app->visitor()->setFlash('L\'adresse email existe déjà!');
+                    $pass = $request->postData('password');
+                    $passCheck = $request->postData('passCheck');
+                    if ($pass !== $passCheck) {
+                        $this->app->visitor()->setFlash('Les mots de passe sont différents!');
                     } else {
-                        $this->managers->getManagerOf('Users')->add($user);
-                        $this->app->visitor()->setFlash('La demande d\'inscription a bien été enregistrée');
+                        $pass = password_hash($pass, PASSWORD_DEFAULT);
+                        
+                        $user = new User([
+                            'name' => $request->postData('name'),
+                            'lastName' => $request->postData('lastName'),
+                            'username' => $request->postData('login'),
+                            'email' => $request->postData('email'),
+                            'password' => $pass,
+                        ]);
+                        $pseudo = $this->managers->getManagerOf('Users')->get($user->username());
+                        $email = $this->managers->getManagerOf('Users')->getEmail($user->email());
+                        if (isset($pseudo) && $pseudo != false) {
+                        $this->app->visitor()->setFlash('Le login n\'est plus disponible');
+                        } elseif (isset($email) && $email != false) {
+                            $this->app->visitor()->setFlash('L\'adresse email existe déjà!');
+                        } else {
+                            $this->managers->getManagerOf('Users')->add($user);
+                            $this->app->visitor()->setFlash('La demande d\'inscription a bien été enregistrée');
+                        }
                     }
                 }
-            }
+            //}
         }
 
     }
